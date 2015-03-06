e7a2494305310a7e1f1a4eecbad05859534ab7c3
Gregory Eremin <g@erem.in>
2015-03-06 19:15:35 +0700

    Cleanup api code using respondWith helper

--- server/api.go
+++ server/api.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/localhots/empact/db"
@@ -10,24 +9,15 @@ import (
 func apiOrgsHandler(w http.ResponseWriter, r *http.Request) {
 	login := currentUser(r)
 	orgs := db.UserOrgs(login)
-	b, _ := json.Marshal(orgs)
-
-	w.Header().Set("Content-Type", "application/json; charset=utf8")
-	w.Write(b)
+	respondWith(w, orgs)
 }
 
 func apiTeamsHandler(w http.ResponseWriter, r *http.Request) {
 	teams := db.OrgTeams(r.FormValue("org"))
-	b, _ := json.Marshal(teams)
-
-	w.Header().Set("Content-Type", "application/json; charset=utf8")
-	w.Write(b)
+	respondWith(w, teams)
 }
 
 func apiReposHandler(w http.ResponseWriter, r *http.Request) {
 	repos := db.OrgRepos(r.FormValue("org"))
-	b, _ := json.Marshal(repos)
-
-	w.Header().Set("Content-Type", "application/json; charset=utf8")
-	w.Write(b)
+	respondWith(w, repos)
 }
