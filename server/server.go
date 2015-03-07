da9ec3482e31ee496f4b2e3903fd027e2685c113
Gregory Eremin <g@erem.in>
2015-03-07 16:44:50 +0700

    Move app from server

--- server/server.go
+++ server/server.go
@@ -2,26 +2,11 @@ package server
 
 import (
 	"encoding/json"
-	"html/template"
 	"log"
 	"net/http"
-
-	"github.com/GeertJohan/go.rice"
-)
-
-var (
-	helloTmpl = template.New("hello")
-	appTmpl   = template.New("app")
-	box       = rice.MustFindBox("app")
 )
 
 func init() {
-	parseTemplate("hello.tmpl", helloTmpl)
-	parseTemplate("app.tmpl", appTmpl)
-
-	// Serving static files
-	http.Handle("/app/", http.StripPrefix("/app/", http.FileServer(box.HTTPBox())))
-
 	http.HandleFunc("/", sessionHandler)
 	http.HandleFunc("/hello", appHelloHandler)
 	http.HandleFunc("/app", appAppHandler)
@@ -43,20 +28,13 @@ func Start() {
 	http.ListenAndServe(":8080", nil)
 }
 
-func parseTemplate(file string, tmpl *template.Template) {
-	if tmplText, err := box.String(file); err == nil {
-		tmpl, _ = tmpl.Parse(tmplText)
-	} else {
-		panic(err)
-	}
-}
-
 func respondWith(w http.ResponseWriter, resp interface{}) {
 	b, err := json.Marshal(resp)
 	if err != nil {
 		panic(err)
 	}
 
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json; charset=utf8")
 	w.Write(b)
 }
