755a8ffe5330ba990a71e22fb92591bc0b75b027
Gregory Eremin <g@erem.in>
2015-03-19 19:49:32 +0700

    Add save method to team model

--- db/team.go
+++ db/team.go
@@ -5,13 +5,26 @@ import (
 )
 
 type Team struct {
-	ID    uint64 `json:"id"`
-	Slug  string `json:"slug"`
-	Owner string `json:"owner"`
-	Name  string `json:"name"`
+	ID         uint64 `json:"id"`
+	Slug       string `json:"slug"`
+	Name       string `json:"name"`
+	Permission string `json:"permission"`
+	OrgID      uint64 `json:"org_id" db:"org_id"`
 }
 
 const orgTeamsQuery = `select * from teams where owner = ?`
+const saveTeamQuery = `
+insert into teams (id, slug, name, permission, org_id, created_at, updated_at)
+values (:id, :slug, :name, :permission, :org_id, now(), now())
+on duplicate key update
+slug = values(slug),
+permission = values(permission),
+updated_at=now()`
+
+func (t *Team) Save() {
+	defer measure("SaveTeam", time.Now())
+	mustExecN(saveTeamQuery, t)
+}
 
 func OrgTeams(login string) (teams []*Team) {
 	defer measure("OrgTeams", time.Now())
